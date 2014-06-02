7c5f0e4ebfb60a35f8fb76535c883639c16f7345
CarlaAstudillo <carla.a.astudillo@gmail.com>
2014-06-02 16:14:08 -0400

    Changed various :string into :text

--- db/migrate/20140516194738_add_morefields_to_posts.rb
+++ db/migrate/20140516194738_add_morefields_to_posts.rb
@@ -1,17 +1,17 @@
 class AddMorefieldsToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :reason_for_initial, :string
-    add_column :posts, :narrative, :string
-    add_column :posts, :threats, :string
+    add_column :posts, :reason_for_initial, :text
+    add_column :posts, :narrative, :text
+    add_column :posts, :threats, :text
     add_column :posts, :weapon, :string
-    add_column :posts, :attack, :string
-    add_column :posts, :law_action, :string
+    add_column :posts, :attack, :text
+    add_column :posts, :law_action, :text
     add_column :posts, :law_weapons, :string
     add_column :posts, :fatal_injury, :string
     add_column :posts, :death_weapons, :string
     add_column :posts, :final_death_place, :string
     add_column :posts, :autopsy, :string
-    add_column :posts, :notes, :string
+    add_column :posts, :notes, :text
     add_column :posts, :death_location_category, :string
     add_column :posts, :death_determined, :string
     add_column :posts, :deceased_crime, :string
